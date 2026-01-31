e105f62343469f4b2ed832216ad25f90217a78e4
Alvin Zhu <alvinzhu@bu.edu>
2026-01-30 20:40:00 -0500

    Fix URL mount for cs-webapps

--- cs412/urls.py
+++ cs412/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cs412/quotes/', include('quotes.urls')),
+    path('/quotes/', include('quotes.urls')),
 ]
