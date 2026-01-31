ce184384b9895c5a740329fca632b5cddd773d02
Alvin Zhu <alvinzhu@bu.edu>
2026-01-30 20:49:47 -0500

    Fix trailing slashes for deployed routes

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.quote, name='home'),
-    path('quote', views.quote, name='quote'),
-    path('show_all', views.show_all, name='show_all'),
-    path('about', views.about, name='about'),
+    path('', views.quote, name='quote'),
+    path('quote/', views.quote, name='quote2'),
+    path('show_all/', views.show_all, name='show_all'),
+    path('about/', views.about, name='about'),
 ]
